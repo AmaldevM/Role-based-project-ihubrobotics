d8c32aba72032b8b49e75fea7ce5ae0fb6ebfcc8
Amaldevm <amaldevhari265@gmail.com>
2025-09-30 16:21:34 +0530

    [feature/Protectedroute] Update improved readability and /login instead of /

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,15 +2,15 @@ import { type JSX } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
-export default function ProtectedRoute({
-  children,
-}: {
+interface ProtectedRouteProps {
   children: JSX.Element;
-}) {
+}
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   const { isAuthenticated } = useAuth();
 
   if (!isAuthenticated()) {
-    return <Navigate to="/" replace />;
+    return <Navigate to="/login" replace />;
   }
 
   return children;
